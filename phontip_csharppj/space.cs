f05d7db6b849108673c6f07869b9ddc98ef76852
phontip <77142242+phontip@users.noreply.github.com>
2021-06-06 10:14:12 +0700

    Add files via upload

---
+++ phontip_csharppj/space.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phontip_csharppj
+{
+    public partial class space : Form
+    {
+        public space()
+        {
+            InitializeComponent();
+        }
+
+        int starttime = 0;
+        private void timer1_Tick(object sender, EventArgs e) //จับเวลาให้ loding-100 ให้ไปหน้า login
+        {
+            starttime += 1;
+            Myprogress.Value = starttime;
+            Percentagelable.Text = starttime + "%";
+
+            if(Myprogress.Value == 100)
+            {
+                Myprogress.Value = 0;
+                timer1.Stop();
+
+                login log = new login();
+                log.Show();
+                this.Hide();
+
+            }
+        }
+
+        private void space_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
