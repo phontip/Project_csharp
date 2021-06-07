445b954828ce82ac8ccc43e60e6c3df872ce5ff7
phontip <77142242+phontip@users.noreply.github.com>
2021-06-07 13:38:17 +0700

    Add files via upload

---
+++ phontip_csharppj/product_stock.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace phontip_csharppj
+{
+    partial class product_stock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(product_stock));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenubarItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Accessories = new System.Windows.Forms.ToolStripMenuItem();
+            this.Bag = new System.Windows.Forms.ToolStripMenuItem();
+            this.Hat = new System.Windows.Forms.ToolStripMenuItem();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.stocklabel1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel7.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1017, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel7.Controls.Add(this.panel2);
+            this.panel7.Controls.Add(this.label11);
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Controls.Add(this.panel5);
+            this.panel7.Controls.Add(this.panel4);
+            this.panel7.ForeColor = System.Drawing.Color.DimGray;
+            this.panel7.Location = new System.Drawing.Point(12, 91);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(274, 598);
+            this.panel7.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Location = new System.Drawing.Point(0, 164);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(271, 434);
+            this.panel2.TabIndex = 18;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Brown;
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(24, 372);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(210, 47);
+            this.panel3.TabIndex = 39;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(60, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 34);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Back";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DimGray;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenubarItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(271, 38);
+            this.menuStrip1.TabIndex = 38;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenubarItem
+            // 
+            this.MenubarItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Accessories,
+            this.Bag,
+            this.Hat});
+            this.MenubarItem.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MenubarItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.MenubarItem.Name = "MenubarItem";
+            this.MenubarItem.Size = new System.Drawing.Size(221, 34);
+            this.MenubarItem.Text = " SHOP PRODUCT";
+            // 
+            // Accessories
+            // 
+            this.Accessories.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Accessories.Name = "Accessories";
+            this.Accessories.Size = new System.Drawing.Size(270, 34);
+            this.Accessories.Text = "Accessories";
+            this.Accessories.Click += new System.EventHandler(this.Accessories_Click);
+            // 
+            // Bag
+            // 
+            this.Bag.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bag.Name = "Bag";
+            this.Bag.Size = new System.Drawing.Size(270, 34);
+            this.Bag.Text = "Bag";
+            this.Bag.Click += new System.EventHandler(this.Bag_Click);
+            // 
+            // Hat
+            // 
+            this.Hat.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hat.Name = "Hat";
+            this.Hat.Size = new System.Drawing.Size(270, 34);
+            this.Hat.Text = "Hat";
+            this.Hat.Click += new System.EventHandler(this.Hat_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label11.Location = new System.Drawing.Point(18, 254);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 34);
+            this.label11.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(18, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(242, 34);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "PRODUCT STOCK";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.IndianRed;
+            this.panel5.Controls.Add(this.stocklabel1);
+            this.panel5.Location = new System.Drawing.Point(21, 71);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(107, 70);
+            this.panel5.TabIndex = 41;
+            // 
+            // stocklabel1
+            // 
+            this.stocklabel1.AutoSize = true;
+            this.stocklabel1.BackColor = System.Drawing.Color.IndianRed;
+            this.stocklabel1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stocklabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.stocklabel1.Location = new System.Drawing.Point(22, 19);
+            this.stocklabel1.Name = "stocklabel1";
+            this.stocklabel1.Size = new System.Drawing.Size(83, 34);
+            this.stocklabel1.TabIndex = 14;
+            this.stocklabel1.Text = "XXXX";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(21, 73);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(229, 68);
+            this.panel4.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(118, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 34);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "piece";
+        
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dataGrid);
+            this.panel1.Location = new System.Drawing.Point(279, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 598);
+            this.panel1.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(61, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 34);
+            this.label1.TabIndex = 19;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(57, 73);
+            this.dataGrid.Name = "dataGrid";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.RosyBrown;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGrid.RowHeadersWidth = 40;
+            this.dataGrid.RowTemplate.Height = 20;
+            this.dataGrid.Size = new System.Drawing.Size(663, 496);
+            this.dataGrid.TabIndex = 58;
+            // 
+            // product_stock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1041, 701);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "product_stock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PRODUCT STOCK";
+            this.Load += new System.EventHandler(this.product_stock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenubarItem;
+        private System.Windows.Forms.ToolStripMenuItem Accessories;
+        private System.Windows.Forms.ToolStripMenuItem Bag;
+        private System.Windows.Forms.ToolStripMenuItem Hat;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label stocklabel1;
+    }
+}
---
+++ phontip_csharppj/product_stock.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+
+namespace phontip_csharppj
+{
+    public partial class product_stock : Form
+    {
+        public product_stock()
+        {
+            InitializeComponent();
+        }
+        
+        private MySqlConnection databaseConnection() //เชื่อมต่อ database
+        {
+            string connectionString = " datasource=127.0.0.1;port=3306;username=root;password=;database=pair;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            return conn;
+
+        }
+        private void Showdata(string valueToSearch) //โชว์ data stock
+        {
+            MySqlConnection conn = databaseConnection();
+            DataSet ds = new DataSet();
+            conn.Open();
+
+            MySqlCommand cmd;
+
+            cmd = conn.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM stock";
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(ds);
+
+
+            dataGrid.DataSource = ds.Tables[0].DefaultView;
+
+
+            conn.Close();
+
+        }
+        MySqlConnection connection = new MySqlConnection(" datasource=127.0.0.1;port=3306;username=root;password=;database=pair;charset=utf8;");
+
+        private void Accessories_Click(object sender, EventArgs e)//menustrip
+        {
+            MySqlDataAdapter da;
+            DataTable dt;
+            connection.Open();
+            da = new MySqlDataAdapter("SELECT * FROM stock WHERE Categories LIKE'" + this.Accessories.Text + "%'", connection);
+            dt = new DataTable();
+            da.Fill(dt);
+
+            dataGrid.AllowUserToAddRows = false;
+            dataGrid.DataSource = dt;
+            connection.Close();
+
+            int numRows = dataGrid.Rows.Count;
+            label1.Text = dataGrid.CurrentRow.Cells[2].Value.ToString()+"  "+ numRows.ToString() + "  piece";
+
+        }
+
+        private void Bag_Click(object sender, EventArgs e)//menustrip
+        {
+            MySqlDataAdapter da;
+            DataTable dt;
+            connection.Open();
+            da = new MySqlDataAdapter("SELECT * FROM stock WHERE Categories LIKE'" + this.Bag.Text + "%'", connection);
+            dt = new DataTable();
+            da.Fill(dt);
+
+            dataGrid.AllowUserToAddRows = false;
+            dataGrid.DataSource = dt;
+            connection.Close();
+
+            int numRows = dataGrid.Rows.Count;
+            label1.Text = dataGrid.CurrentRow.Cells[2].Value.ToString()+"  "+ numRows.ToString() + "  piece";
+
+            
+        }
+
+        private void Hat_Click(object sender, EventArgs e)//menustrip
+        {
+
+            MySqlDataAdapter da;
+            DataTable dt;
+            connection.Open();
+            da = new MySqlDataAdapter("SELECT * FROM stock WHERE Categories LIKE'" + this.Hat.Text + "%'", connection);
+            dt = new DataTable();
+            da.Fill(dt);
+
+            dataGrid.AllowUserToAddRows = false;
+            dataGrid.DataSource = dt;
+            connection.Close();
+
+            int numRows = dataGrid.Rows.Count;
+            label1.Text = dataGrid.CurrentRow.Cells[2].Value.ToString()+"  "+ numRows.ToString() + "  piece";
+        }
+
+        private void product_stock_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+            MySqlDataAdapter sda = new MySqlDataAdapter("select sum(Quantity) from pair", connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            stocklabel1.Text = dt.Rows[0][0].ToString();//เอาจํานวน สินค้า
+            connection.Close();
+
+            Showdata("SELECT * FROM stock");
+
+        }
+
+        private void panel3_Click(object sender, EventArgs e)//กลับไปหน้าstatistic
+        {
+            Statistic Obj = new Statistic();
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
