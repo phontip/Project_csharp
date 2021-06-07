8ebef82bd3d151fbad9d94e7716bd57151bc983d
phontip <77142242+phontip@users.noreply.github.com>
2021-06-07 13:37:07 +0700

    Add files via upload

---
+++ phontip_csharppj/ttuser.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace phontip_csharppj
+{
+    partial class ttuser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ttuser));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.stocklabel1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stocklabel1
+            // 
+            this.stocklabel1.AutoSize = true;
+            this.stocklabel1.BackColor = System.Drawing.Color.IndianRed;
+            this.stocklabel1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stocklabel1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.stocklabel1.Location = new System.Drawing.Point(43, 20);
+            this.stocklabel1.Name = "stocklabel1";
+            this.stocklabel1.Size = new System.Drawing.Size(49, 34);
+            this.stocklabel1.TabIndex = 14;
+            this.stocklabel1.Text = "XX";
+           
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(14, 20);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(229, 68);
+            this.panel4.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(133, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 34);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "user";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(67, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 39);
+            this.label4.TabIndex = 59;
+            this.label4.Text = "User List";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel1.Controls.Add(this.dataGrid);
+            this.panel1.Location = new System.Drawing.Point(279, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(846, 598);
+            this.panel1.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label11.Location = new System.Drawing.Point(18, 254);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 34);
+            this.label11.TabIndex = 17;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.IndianRed;
+            this.panel5.Controls.Add(this.stocklabel1);
+            this.panel5.Location = new System.Drawing.Point(14, 20);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(107, 68);
+            this.panel5.TabIndex = 41;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(60, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 34);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Back";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Brown;
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(24, 452);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(210, 47);
+            this.panel3.TabIndex = 39;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(0, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(271, 528);
+            this.panel2.TabIndex = 18;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel7.Controls.Add(this.panel2);
+            this.panel7.Controls.Add(this.label4);
+            this.panel7.Controls.Add(this.label11);
+            this.panel7.ForeColor = System.Drawing.Color.DimGray;
+            this.panel7.Location = new System.Drawing.Point(12, 91);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(274, 598);
+            this.panel7.TabIndex = 18;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1113, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(67, 53);
+            this.dataGrid.Name = "dataGrid";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.RosyBrown;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGrid.RowHeadersWidth = 40;
+            this.dataGrid.RowTemplate.Height = 20;
+            this.dataGrid.Size = new System.Drawing.Size(726, 496);
+            this.dataGrid.TabIndex = 59;
+            // 
+            // ttuser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 701);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ttuser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User";
+            this.Load += new System.EventHandler(this.ttuser_Load);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label stocklabel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
---
+++ phontip_csharppj/ttuser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace phontip_csharppj
+{
+    public partial class ttuser : Form
+    {
+        public ttuser()
+        {
+            InitializeComponent();
+        }
+      
+        MySqlConnection conn = new MySqlConnection(" datasource=127.0.0.1;port=3306;username=root;password=;database=pair;charset=utf8;");
+        
+        private void Showdata(string valueToSearch) //โชว์ data user
+        {
+            
+            DataSet ds = new DataSet();
+            conn.Open();
+
+            MySqlCommand cmd;
+
+            cmd = conn.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM user";
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(ds);
+
+
+            dataGrid.DataSource = ds.Tables[0].DefaultView;
+
+
+            conn.Close();
+
+        }
+      
+
+        private void ttuser_Load(object sender, EventArgs e)
+        {
+            Showdata("SELECT * FROM user");
+
+            MySqlDataAdapter sda2 = new MySqlDataAdapter("select Count(*) from user", conn);
+            DataTable dt2 = new DataTable();
+            sda2.Fill(dt2);
+            stocklabel1.Text = dt2.Rows[0][0].ToString();//เอาจํานวนผู้ใช้
+            conn.Close();
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            Statistic Obj = new Statistic();
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
